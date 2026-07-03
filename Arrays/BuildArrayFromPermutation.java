f002fb3626adca71248694e9b02905eae2f701bc
Saksham Tiwari <sakshamt1304@gmail.com>
2026-07-03 22:18:01 +0530

    Add Build Array from Permutation solution

---
+++ Arrays/BuildArrayFromPermutation.java
@@ -0,0 +1,18 @@
+/*
+LeetCode 1920
+Build Array from Permutation
+Time : O(n)
+Space : O(n)
+*/
+
+class Solution {
+    public int[] buildArray(int[] nums) {
+        int[] ans = new int[nums.length];
+
+        for(int i = 0; i < nums.length; i++) {
+            ans[i] = nums[nums[i]];
+        }
+
+        return ans;
+    }
+}
