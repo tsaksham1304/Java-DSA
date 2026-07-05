9d6c21de50cdcfb99a38e4b6e98ebce261cc22f8
Saksham Tiwari <sakshamt1304@gmail.com>
2026-07-05 23:24:04 +0530

    Add Max Consecutive Ones solution

---
+++ Arrays/MaxConsecutiveOnes.java
@@ -0,0 +1,25 @@
+/*
+LeetCode 485
+Max Consecutive Ones
+Time : O(m x n)
+Space : O(1)
+*/
+
+class Solution {
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int maxCount = 0;
+        int count=0;
+        for(int i=0;i<nums.length;i++){
+            if (nums[i]==1){
+                count++;
+            }
+            else{
+                count=0;
+            }
+            if (count>maxCount){
+                maxCount = count;
+            }
+        }
+        return maxCount;
+    }
+}
