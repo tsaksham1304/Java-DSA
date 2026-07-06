f10e55eb7db347be50e44e5c0e9a4febb036cda7
Saksham Tiwari <sakshamt1304@gmail.com>
2026-07-06 16:38:48 +0530

    Add Remove Duplicates from Sorted Array solution

---
+++ Arrays/RemoveDuplicatesFromSortedArray.java
@@ -0,0 +1,19 @@
+/*
+LeetCode 485
+Remove Duplicates from Sorted Array
+Time : O(n)
+Space : O(1)
+*/
+
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int write=0;
+        for(int read=1;read<nums.length;read++){
+            if(nums[write]!=nums[read]){
+                write++;
+                nums[write] = nums[read];
+            }
+        }
+        return write+1;
+    }
+}
